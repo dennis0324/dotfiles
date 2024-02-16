1641132f1407c949c9a8b863d762388b4f639744
dennis ko <dennis0324@naver.com>
2024-02-16 21:45:52 +0900

    add: i3, zsh commands

---
+++ dot_config/zsh/command/rebootwin.zsh
@@ -0,0 +1,3 @@
+function rebootwin {
+  /home/dennis/.dotfiles/reboot/rebootwin
+}
---
+++ dot_config/zsh/command/vencord.zsh
@@ -0,0 +1,3 @@
+function vencord {
+  sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)" 
+}
