6b7fa0aa5133824cd0b13baa7e11bd23d721c505
dennis ko <dennis0324@naver.com>
2024-02-17 22:25:40 +0900

    add: chemzoi auto script

---
+++ run_once_0_install_i3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+install_on_arch() {
+	pacman -S i3 --noconfirm
+}
+
+OS="$(uname -s)"
+case "${OS}" in
+Linux*)
+	if [ -f /etc/arch-release ]; then
+		install_on_fedora
+	else
+		echo "Skipping i3 due to linux distribution"
+		exit 1
+	fi
+	;;
+*)
+	echo "Skipping i3 due to unsupported operating system: ${OS}"
+	exit 1
+	;;
+esac
+
+echo "Ansible installation complete."
