adfa9dc0ae2211845bb1ac6c12162b1bb44a4b42
dennis ko <dennis0324@naver.com>
2024-02-18 18:59:08 +0900

    change: running seq

--- run_once_1_install_ohmyzsh.sh
+++ run_once_1_install_ohmyzsh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-	echo "Getting ohmyz.sh"
+	echo "installing ohmyz.sh"
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
 else
 	echo ".oh-my-zsh already found, skipping."
