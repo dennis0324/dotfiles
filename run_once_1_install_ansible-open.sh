d2d720789b7f737ebacd08536e01f8c1346e5372
dennis ko <dennis0324@naver.com>
2024-03-18 14:28:43 +0900

    add: git add open, wrapup

---
+++ run_once_1_install_ansible-open.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Ansible installing..."
+
+install_on_arch() {
+	sudo pacman -S ansible --noconfirm
+}
+install_on_mac() {
+	brew install ansible
+}
+
+OS="$(uname -s)"
+case "${OS}" in
+Linux*)
+	if [ -f /etc/arch-release ]; then
+		install_on_arch
+	else
+		echo "Unsupported Linux distribution"
+		exit 1
+	fi
+	;;
+Darwin*)
+	install_on_mac
+	;;
+*)
+	echo "Unsupported operating system: ${OS}"
+	exit 1
+	;;
+esac
+
+ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass
+
+echo "Ansible installation complete."
---
+++ run_once_1_install_ansible-wrapup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "[password for vault]"
+pass_yml=$(ansible-vault view ~/.bootstrap/secret.yml)
+pass=(${pass_yml//:/})
+if [[ -f ~/.secret/pw/wheel ]]; then
+	touch ~/.secret/pw/wheel
+fi
+echo ${pass[2]} >~/.secret/pw/wheel
+# ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass
