977f174ace26debad26ffb75f87523b88869366b
dennis ko <dennis0324@naver.com>
2024-03-21 21:25:18 +0900

    change: wrapup

--- run_once_1_install_ansible-wrapup.sh
+++ run_once_1_install_ansible-wrapup.sh
@@ -3,8 +3,10 @@
 echo "[password for vault]"
 pass_yml=$(ansible-vault view ~/.bootstrap/secret.yml)
 pass=(${pass_yml//:/})
-if [[ -f ~/.secret/pw/wheel ]]; then
+if [[ -d ~/.secret/pw/ ]]; then
 	mkdir -p ~/.secret/pw
+fi
+if [[ -f ~/.secret/pw/wheel ]]; then
 	touch ~/.secret/pw/wheel
 fi
 echo ${pass[2]} >~/.secret/pw/wheel
