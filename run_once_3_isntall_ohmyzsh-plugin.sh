79477accd0533dd31c297b9631b23a76f092c187
dennis ko <dennis0324@naver.com>
2024-03-21 21:20:48 +0900

    change: git path

--- run_once_3_isntall_ohmyzsh-plugin.sh
+++ run_once_3_isntall_ohmyzsh-plugin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autocomplete/ ]; then
-	git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ~/.oh-my-zsh/custom/plugins/
+	git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ~/.oh-my-zsh/custom/plugins/zsh-autocomplete/
 fi
