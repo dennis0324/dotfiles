13181d76807b338a9840d7da0b3bc72bc51bc32d
dennis ko <dennis0324@naver.com>
2024-03-22 14:09:47 +0900

    feat: installing yay

---
+++ run_once_4_install_yay.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# install ayay
+
+if command -v yay &>/dev/null; then
+	if [ ! -d ~/tmp ]; then
+		mkdir ~/tmp/
+	fi
+	git clone https://aur.archlinux.org/yay.git ~/tmp
+	cd ~/tmp/yay
+	makepkg -si
+	rm -rf ~/tmp/yay
+fi
